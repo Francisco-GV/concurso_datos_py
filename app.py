13dd02f94c8daebdc8ffeb31359ec0b139c10c80
Francisco Gasca <francisco_gasca@hotmail.com>
2024-11-16 14:49:52 -0600

    Add basic multi-page menu

--- app.py
+++ app.py
@@ -1,2 +1,17 @@
+import dash
+from dash import html, dcc
+
+app = dash.Dash(__name__, use_pages=True)
+
+app.layout = html.Div([
+    html.H1('Multi-page app with Dash Pages'),
+    html.Div([
+        html.Div(
+            dcc.Link(f"{page['name']} - {page['path']}", href=page["relative_path"])
+        ) for page in dash.page_registry.values()
+    ]),
+    dash.page_container
+])
+
 if __name__ == "__main__":
-    print("Hello World")
+    app.run(debug=True)
--- pages/advisor_feedback_page.py
+++ pages/advisor_feedback_page.py
@@ -1,8 +1,9 @@
-from dash import Dash, html, dcc, callback, Output, Input
+import dash
+from dash import html, dcc, callback, Output, Input
 import plotly.express as px
 
-from ..data.analysis import advisor_feedback as af
-from ..data import data_preprocessor as dp
+from data.analysis import advisor_feedback as af
+from data import data_preprocessor as dp
 
 csv_path = "C:\\Dev\\ConcursoDatos\\test\\Satisfacción de servicio para UPG 2024.csv"
 df = dp.load_file(csv_path)
@@ -13,9 +14,9 @@ names = af.get_advisor_names(advisor_df, names_range)
 
 advisor_df = af.melt(advisor_df, questions, names, "Asesores")
 
-app = Dash()
+dash.register_page(__name__)
 
-app.layout = [
+layout = [
     html.H1(children="Asesores", style={"textAlign":"center"}),
     dcc.Dropdown(questions, questions[0], id="dropdown-questions"),
     dcc.Dropdown(names, names[0], id="dropdown-names"),
@@ -33,5 +34,3 @@ def update_graph(question, name):
     dff = af.count_qualitative_responses(dff, "Asesores", question, count_column_name)
     return px.bar(dff, x=question, y=count_column_name)
 
-if __name__ == "__main__":
-    app.run(debug=True)
