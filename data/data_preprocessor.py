7131e06c6aae76c12bc993d9cfb7d346c75ac9c1
Francisco Gasca <francisco_gasca@hotmail.com>
2024-11-20 22:39:24 -0600

    Convert date columns to datetime type

--- data/data_preprocessor.py
+++ data/data_preprocessor.py
@@ -11,3 +11,12 @@ def load_file(path):
 def load_global_df():
     global df_global
     df_global = load_file(csv_path)
+
+    preprocess_global_df()
+
+
+def preprocess_global_df():
+    # Convierte las columnas de fecha en tipo datetime para realizar operaciones con fechas
+    df_global["date_created"] = pd.to_datetime(df_global["date_created"], format="mixed")
+    df_global["date_modified"] = pd.to_datetime(df_global["date_modified"], format="mixed")
+
